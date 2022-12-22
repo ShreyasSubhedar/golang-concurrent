b79acd7d14c41c504d2639288a29a2103588bae7
ss-stackid <shreyas.subhedar@stackidentity.com>
2022-12-22 11:47:12 +0530

    SP-0 | New way to add mutex

--- main.go
+++ main.go
@@ -7,85 +7,61 @@ import (
 	"time"
 )
 
-type Data struct {
-	GlobalLock sync.RWMutex
-	Cache      map[int]Book
-}
-
 var (
-	rnd  = rand.New(rand.NewSource(time.Now().UnixNano()))
-	data = Data{}
+	rnd   = rand.New(rand.NewSource(time.Now().UnixNano()))
+	cache = map[int]Book{}
 )
 
 func main() {
-	data.Cache = make(map[int]Book, 0)
 	wg := &sync.WaitGroup{}
+	m := &sync.Mutex{}
 	for i := 0; i < 10; i++ {
 		id := rnd.Intn(10) + 1
 
 		wg.Add(2)
-		go func(id int, wg *sync.WaitGroup) {
+		go func(id int, wg *sync.WaitGroup, m *sync.Mutex) {
 			defer wg.Done()
-			book, val := queryCache(id)
+			book, val := queryCache(id, m)
 			if val {
 				fmt.Println("--------------Getting from Cache-----------------------------")
 				fmt.Println(book)
 
 			}
 
-		}(id, wg)
+		}(id, wg, m)
 
-		go func(id int, wg *sync.WaitGroup) {
+		go func(id int, wg *sync.WaitGroup, m *sync.Mutex) {
 			defer wg.Done()
-			book, val := queryFromDB(id)
+			book, val := queryFromDB(id, m)
 			if val {
 				fmt.Println("--------------Getting from DB-----------------------------")
 				fmt.Println(book)
 			}
-		}(id, wg)
+		}(id, wg, m)
+		time.Sleep(150 * time.Millisecond)
 	}
 	wg.Wait()
 }
-func queryCache(id int) (Book, bool) {
-
-	if b, ok := data.GetCacheLock(id); ok {
+func queryCache(id int, m *sync.Mutex) (Book, bool) {
+	m.Lock()
+	b, ok := cache[id]
+	m.Unlock()
+	if ok {
 		return b, true
 	}
+
 	return Book{}, false
 }
 
-func queryFromDB(id int) (Book, bool) {
-	// time.Sleep(2 * time.Millisecond)
+func queryFromDB(id int, m *sync.Mutex) (Book, bool) {
+	time.Sleep(100 * time.Millisecond)
 	for _, b := range books {
 		if b.ID == id {
-			data.AddCacheLock(b.ID, b)
+			m.Lock()
+			cache[id] = b
+			m.Unlock()
 			return b, true
 		}
 	}
 	return Book{}, false
-}
-
-// ---------------Mutex Un/Locks----------------
-
-func (d *Data) GetCacheLock(id int) (Book, bool) {
-	d.GlobalLock.Lock()
-	defer d.GlobalLock.Unlock()
-	return d.getCachecNoLock(id)
-}
-
-func (d *Data) getCachecNoLock(id int) (Book, bool) {
-	if book, ok := d.Cache[id]; ok {
-		return book, ok
-	}
-	return Book{}, false
-}
-
-func (d *Data) AddCacheLock(id int, book Book) {
-	d.GlobalLock.Lock()
-	defer d.GlobalLock.Unlock()
-	d.addCachecNoLock(id, book)
-}
-
-func (d *Data) addCachecNoLock(id int, book Book) {
-	d.Cache[id] = book
 }
